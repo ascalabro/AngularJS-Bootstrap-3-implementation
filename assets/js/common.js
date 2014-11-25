d730627f6f98301423dfd0fbd3d7528ad39f35b1
Angelo Scalabroni <armotek5421@msn.com>
2014-11-25 12:20:49 -0500

    get latest bootstrap(v3.3.1); clean up code, reorganize files; fix broken references

---
+++ assets/js/common.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+    // This is needed to collapse the navbar
+    $(".navbar-toggle").click(function(event) {
+        $(".navbar-collapse").toggle('in');
+    });
+
+});
