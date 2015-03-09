c629758a0c8e8c8d1072e14d566d2a9a0ecf6fe6
Angelo Scalabroni <ascalabroni@1vmserver.(none)>
2015-03-09 15:51:41 -0400

    style headers on listings page

--- app/controllers/storeListingsCtrl.js
+++ app/controllers/storeListingsCtrl.js
@@ -1,4 +1,4 @@
 app.controller('storeListingsCtrl', function($scope, $stateParams, listingFactory) {
-    $scope.firstName = 'storeListingsCtrl firstname';
     $scope.listings = listingFactory.list.byCategory({category_id: $stateParams.category_id});
+//    $scope.categoryName = $scope.listings;
 });;
